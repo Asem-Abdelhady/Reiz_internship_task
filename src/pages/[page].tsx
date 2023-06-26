72d5e412f463c98903ae2de691f254788d3c05cb
Asem Abdelhady <asemshawkey@gmail.com>
2023-06-26 22:07:54 +0300

    Change: to useEffect

--- src/pages/[page].tsx
+++ src/pages/[page].tsx
@@ -14,7 +14,7 @@ import axios from "axios";
 
 import CountriesList from "@/components/CountriesList/CountriesList";
 import CustomPagination from "@/components/Pagination/Pagination";
-import { useReducer, useState } from "react";
+import { useEffect, useReducer, useState } from "react";
 import { useHandleOperation } from "@/Context/CountriesContext";
 import { useRouter } from "next/router";
 
@@ -38,14 +38,13 @@ function getTenPages(
 ): CountryResponse[] {
   return countries.slice(start, start + 10);
 }
-export default function Home({
-  data,
-}: InferGetStaticPropsType<typeof getStaticProps>) {
+export default function Home() {
   const router = useRouter();
   const handleOperation = useHandleOperation();
 
   let initStart = ((router.query.page as unknown as number) - 1) * 10;
-  const [start, setStart] = useState<number>(initStart);
+  const [data, setData] = useState<CountryResponse[]>([]);
+  const [start, setStart] = useState<number>(initStart ? initStart : 0);
   const [currentList, setCurrentList] = useState<CountryResponse[]>(
     getTenPages(data, start)
   );
@@ -96,6 +95,16 @@ export default function Home({
     },
     initalState
   );
+
+  useEffect(() => {
+    const URL = `${BASE_URL}/all?fields=name,region,area,flags,population,timezones,currencies,maps,independent`;
+    fetch(URL)
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+        setCurrentList(getTenPages(data, start));
+      });
+  }, []);
 
   return (
     <>
@@ -175,28 +184,3 @@ export default function Home({
     </>
   );
 }
-
-export async function getStaticPaths() {
-  const pages: { params: { page: string } }[] = [];
-  for (let i = 1; i <= 25; i++) {
-    pages.push({ params: { page: String(i) } });
-  }
-  return {
-    paths: pages,
-    fallback: false,
-  };
-}
-
-export const getStaticProps: GetStaticProps<{
-  data: CountryResponse[];
-}> = async (context) => {
-  const URL = `${BASE_URL}/all?fields=name,region,area,flags,population,timezones,currencies,maps,independent`;
-  const res = await axios.get<CountryResponse[]>(URL);
-  const data = res.data;
-
-  return {
-    props: {
-      data,
-    },
-  };
-};
